2cfc89ceb3a5d2567d26f3250b810d0d5dd16fd0
qqqdendi <qqqdendi@gmail.com>
2024-04-02 07:08:05 +0600

    initial commit

---
+++ js.js
@@ -0,0 +1,53 @@
+
+// let arr = ["Chika", "Raatbek", "Kairat"];
+
+// for(let i = 0; i < arr.length; i++){
+//     alert( arr[i]  );
+// }
+
+
+// let x = "6";
+// switch (x){
+//     case "1": alert("Здесь 1");
+//     break;
+//     case "2": alert("Здесь 2");
+//     break;
+//     case "3": alert("Здесь 3");
+//     break;
+//     case "4": alert("Здесь 4");
+//     break;
+//     default: alert("Не совпало");
+// }
+
+// var x = prompt('Введите первое число', 100);
+// var y = prompt('Введите второе число', 100);
+// let e = prompt('Введите 1 что бы сработал знак +, введите 2 чтобы сработал знак -, введите 3 что бы сработал знак *, введите 4 что бы сработал знак /', 100);
+
+// var x = parseInt(x);
+// var y = parseInt(y);
+
+// var result;
+
+// switch(e){
+//     case "1": result = x+y;
+//     break;
+//     case "2": result = x-y;
+//     break;
+//     case "3": result = x*y;
+//     break;
+//     case "4": result = x/y;
+//     break;
+// }
+// alert(result);
+
+
+
+function calcSumm(numOne, numTwo){
+    console.log('Var numOne:${numOne}');
+    console.log('Var numTwo:${numTwo}');
+
+    let numSumm = numOne + numTwo;
+
+    console.log('Summa:${numSumm}');
+}
+calcSumm(1,2);
